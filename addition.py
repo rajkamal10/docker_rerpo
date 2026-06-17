f32222c8b14381d27a8631ac2f2617ed98e0f769
rajkamal10 <48581688+rajkamal10@users.noreply.github.com>
2026-06-17 20:10:56 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,7 +1,6 @@
 def add_numbers(a, b):
     return a + b
-
 if __name__ == "__main__":
     num1 = 10
     num2 = 20
-    print(f"The sum of {num1} and {num2} is {add_numbers(num1, num2)}")
+    print(f"The sum is {add_numbers(num1, num2)}")
